d9ccf196399f0d2aed69012475c30ed426aefbf1
AndongTuomaining <65362645+AndongTuomaining@users.noreply.github.com>
2020-07-07 21:48:37 +0300

    uijuma
    follatkaa hubis

---
+++ stringine.py
@@ -0,0 +1,9 @@
+# Welcome to Stringine.py, this is the main file in the Python version of the Stringine project.
+
+# if something goes wrong it will return None
+
+# Imports for other files:
+from lib.replace import main as replace
+from lib.replace import replaceCaseInsensitive
+from lib.substring import substringStart
+from lib.substring import substringEnd

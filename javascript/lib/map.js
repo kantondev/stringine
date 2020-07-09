4ea2cebdb822e6f0e9790730dcd5957e6b914ad8
botboi37 <atteolkkonen@hotmail.com>
2020-07-09 14:31:05 +0300

    Added some default maps for users to use

---
+++ javascript/lib/map.js
@@ -0,0 +1,56 @@
+/**
+ * Here are maps for the translation & random character functions to help developers save time.
+ * 
+ * For example, if you want to generate a sequence of random numbers you can use stringine.charMap.numbers as the map.
+ */
+var maps = {
+
+    char: {
+
+        "az": ['a','b','c','d','e','f','g','h',
+        'i','j','k','l','m','n', 'o','p','q','r',
+        's','t','u','v','w','x','y','z'],
+      
+        "az_numbers": ['0','1','2','3','4','5','6','7','8','9',
+        'a','b','c','d','e','f','g','h',
+        'i','j','k','l','m','n', 'o','p','q','r',
+        's','t','u','v','w','x','y','z'],
+      
+        "numbers": ['0','1','2','3','4','5','6','7','8','9']
+      
+    },
+
+    translate: {
+        "default": {
+            "a": "#",
+            "b": "&",
+            "c": "=",
+            "d": "/",
+            "e": "%",
+            "f": "}",
+            "g": "€",
+            "h": "£",
+            "i": "_",
+            "j": "{",
+            "k": "^",
+            "l": "¤",
+            "m": "$",
+            "n": "+",
+            "o": "~",
+            "p": ".",
+            "q": ",",
+            "r": "<",
+            "s": ">",
+            "t": "`",
+            "u": "´",
+            "v": "[",
+            "w": "]",
+            "x": "§",
+            "y": "½",
+            "z": ";"
+        }
+    }
+
+}
+
+module.exports = maps;

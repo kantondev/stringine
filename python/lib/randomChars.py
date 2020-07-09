8ea763567e866ab3d502017632632d54049aa45d
botboi37 <atteolkkonen@hotmail.com>
2020-07-09 14:28:04 +0300

    Add try/except block for safety

--- python/lib/randomChars.py
+++ python/lib/randomChars.py
@@ -1,9 +1,12 @@
 def main(length, available):
-  import random
-  result = ""
-  for i in range(length):
-    item = available[random.randint(0,length-1)]
-    if len(item) == 1:
-        result = result + item
-    else: return None
-  return result
+  try:
+    import random
+    result = ""
+    for i in range(length):
+      item = available[random.randint(0,length-1)]
+      if len(item) == 1:
+          result = result + item
+      else: return None
+    return result
+  except:
+    return None

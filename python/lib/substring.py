a50422f60dc5ea04351f456c98c1ce39915b2d56
botboi37 <atteolkkonen@hotmail.com>
2020-07-09 17:02:23 +0300

    Removed number argument from substring functions

--- python/lib/substring.py
+++ python/lib/substring.py
@@ -3,29 +3,17 @@ sys.path.append(os.path.dirname(__file__))
 
 from isValidNumber import main as validNumber
 
-def substringStart(string, thing):
-    if validNumber(thing):
-        try:
-            return (string[0:thing]) 
-        except:
-            return None
-    else:
-        try:
-            if string.startswith(thing):
-                return (string[len(thing):])
-                
-        except:
-            return None
+def substringStart(string, search):
+    try:
+        if string.startswith(search):
+            return (string[len(search):])
+            
+    except:
+        return None
 
-def substringEnd(string, thing):
-    if validNumber(thing):
-        try:
-            return (string[-thing:])
-        except:
-            return None
-    else:
-        try:
-            if string.endswith(thing):
-                return (string[0:(len(string) - len(thing))])
-        except:
-            return None
+def substringEnd(string, search):
+    try:
+        if string.endswith(search):
+            return (string[0:(len(string) - len(search))])
+    except:
+        return None

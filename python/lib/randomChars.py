5285b589b1673e287ea5f591cfab1dd7a6d784a3
AndongTuomaining <65362645+AndongTuomaining@users.noreply.github.com>
2020-07-09 16:54:48 +0300

    Fixed an error

--- python/lib/randomChars.py
+++ python/lib/randomChars.py
@@ -1,12 +1,12 @@
+import math
 def main(length, available):
   try:
     import random
     result = ""
     for i in range(length):
-      item = available[random.randint(0,length-1)]
+      item = available[math.floor(random.random()*len(available))]
       if len(item) == 1:
           result = result + item
       else: return None
     return result
-  except:
-    return None
+  except: return None
